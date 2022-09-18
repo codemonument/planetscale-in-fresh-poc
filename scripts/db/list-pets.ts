c6f6492b7acc1bd7eb874617f01bae48c2835fd6
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-09-19 01:02:44 +0200

    refine reset and list scripts

--- scripts/db/list-pets.ts
+++ scripts/db/list-pets.ts
@@ -2,9 +2,8 @@
 // since normally it's transitively used by kysely-planetscale without the `npm:` identifier
 // TODO: File a bug for npm specifiers, that they should be downloaded by deno even when used in an import-map!
 import {} from "npm:@planetscale/database@1.3.0";
-
 import { db } from "@/src/db/index.ts";
-const result = await db.selectFrom("pets").selectAll().execute(db);
 
+const result = await db.selectFrom("pets").selectAll().execute(db);
 console.log("\n Pets Table");
 console.table(result);
--- scripts/db/reset-pets.ts
+++ scripts/db/reset-pets.ts
@@ -1,12 +1,23 @@
+// Added this line to force download of this npm package,
+// since normally it's transitively used by kysely-planetscale without the `npm:` identifier
+// TODO: File a bug for npm specifiers, that they should be downloaded by deno even when used in an import-map!
+import {} from "npm:@planetscale/database@1.3.0";
+
 import { db } from "@/src/db/index.ts";
 import { sql } from "kysely";
 
 const truncatePets = sql<void>`TRUNCATE TABLE pets`;
-
 await truncatePets.execute(db);
 
-// db
-//   .insertInto("pets")
-//   .values({ first_name: "Jennifer", gender: "female" })
-//   .returning("id")
-//   .executeTakeFirstOrThrow();
+await db
+  .insertInto("pets")
+  .values([
+    { id: 1, name: "Happy" },
+    { id: 2, name: "Moeve" },
+    { id: 3, name: "Bob" },
+  ])
+  .executeTakeFirstOrThrow();
+
+const result = await db.selectFrom("pets").selectAll().execute(db);
+console.log("\n Pets Table");
+console.table(result);
