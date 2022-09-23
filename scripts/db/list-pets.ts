35326d4e75e8f593cd7b28550415074aa432aa77
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-09-23 20:29:24 +0200

    simplify list-pets and reset-pets scripts

--- scripts/db/list-pets.ts
+++ scripts/db/list-pets.ts
@@ -1,7 +1,3 @@
-// Added this line to force download of this npm package,
-// since normally it's transitively used by kysely-planetscale without the `npm:` identifier
-// TODO: File a bug for npm specifiers, that they should be downloaded by deno even when used in an import-map!
-import {} from "npm:@planetscale/database@1.3.0";
 import { db } from "@/src/db/index.ts";
 
 const result = await db.selectFrom("pets").selectAll().execute(db);
