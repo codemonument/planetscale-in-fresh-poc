3782a1c1bd3a89767a7fd5aa76c434df006ba84f
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-15 03:10:42 +0200

    add a fucntion to get secrets from doppler and memoize them in memory

--- src/secrets.ts
+++ src/secrets.ts
@@ -1,9 +1,28 @@
 import { DopplerService } from "doppler_client";
 
-// will be filled by doppler itself for local development & by deno deploy env vars in prod
-const token = Deno.env.get("DOPPLER_TOKEN");
-if (!token) throw new Error(`Missing DOPPLER_TOKEN env var!`);
+let secretsMemo: any;
 
-const doppler = new DopplerService({ token });
-const secrets = await doppler.getConfigs("planetscale-in-deno");
-console.log(secrets);
+export async function secrets(forceRefresh?: boolean) {
+  if (!forceRefresh && secretsMemo) return secretsMemo;
+
+  // will be filled by doppler itself for local development & by deno deploy env vars in prod
+  const token = Deno.env.get("DOPPLER_TOKEN");
+  if (!token) throw new Error(`Missing DOPPLER_TOKEN env var!`);
+
+  const DOPPLER_PROJECT = "planetscale-in-deno";
+
+  const doppler = new DopplerService({ token });
+  const { configs } = await doppler.getConfigs(DOPPLER_PROJECT);
+
+  const rootConfig = configs.find((config) => config.root === true) ||
+    configs[0];
+
+  const { secrets }: any = await doppler.getSecrets(
+    DOPPLER_PROJECT,
+    rootConfig.name,
+  );
+
+  // memoize secrets
+  secretsMemo = secrets;
+  return secrets;
+}
